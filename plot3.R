caa194133d5f51b45f1aa54a62759cd4603434ed
Sachi7 <57734189+Sachi7@users.noreply.github.com>
2020-01-24 12:41:06 -0800

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -16,10 +16,11 @@ mydata <- cbind(date_time, mydata[,3:9])
 
 # Plot 3
 
+png("plot3.png", width=480, height=480)
+
 with(mydata, plot(date_time, Sub_metering_1, type="l", xlab = "", ylab = "Energy sub metering"))
 with(mydata, points(date_time, Sub_metering_2, type = "l", col = "red"))
 with(mydata, points(date_time, Sub_metering_3, type = "l", col = "blue"))
 legend("topright", col = c("black","red", "blue"), legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"))
 
-dev.copy(png, file = "plot3.png", height = 480, width = 480)
 dev.off()
