7f9ee0274fa8038e3c80c87b636041b24689e5d0
Sachi7 <57734189+Sachi7@users.noreply.github.com>
2020-01-24 11:20:36 -0800

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -3,6 +3,7 @@
 
 
 # Reading, cleaning, and subsetting the data
+
 data <- read.table("household_power_consumption.txt", header = TRUE,  sep = ";", na.strings = "?")
 date <- as.Date(data$Date, format = "%d/%m/%Y")
 data_transf <- cbind(date,data[-1])
@@ -13,6 +14,7 @@ mydata <- cbind(data_time, mydata[,3:9])
 ```
 
 # Plot 2
+
 with(mydata, plot(Global_active_power ~ data_time, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)"))
 
 dev.copy(png, file = "plot1.png", height = 480, width = 480)
