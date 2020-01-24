0891d4395166c572ff7539a394ba71ce152ca6a3
Sachi7 <57734189+Sachi7@users.noreply.github.com>
2020-01-24 13:31:47 -0800

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,3 +1,17 @@
+# Exploratory Data Anaylsis
+## Course Project 1
+
+
+# Reading, cleaning, and subsetting the data
+
+data <- read.table("household_power_consumption.txt", header = TRUE,  sep = ";", na.strings = "?")
+date <- as.Date(data$Date, format = "%d/%m/%Y")
+data_transf <- cbind(date,data[-1])
+
+mydata <- subset(data_transf, date >= "2007-02-01" & date <= "2007-02-02")
+data_time <- strptime(paste(mydata$date, mydata$Time), format = "%Y-%m-%d %H:%M:%S")
+mydata <- cbind(data_time, mydata[,3:9])
+
 
 # Plot 4
 
